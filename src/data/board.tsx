60f83a8d9f853cc07ce679b269e4fee20ffa1f8e
manishharit <manishharit974@gmail.com>
2024-05-30 23:49:10 +0530

    first commit

---
+++ src/data/board.tsx
@@ -0,0 +1,132 @@
+import { v4 as uuidv4 } from "uuid";
+import taskImage from "../assets/images/task.jpg";
+import taskImage2 from "../assets/images/task2.jpg";
+import taskImagemani from "../assets/images/taskkk.jpg";
+import taskImage3 from "../assets/images/task3.jpg";
+import { Columns } from "../types";
+import { getRandomColors } from "../helpers/getRandomColors";
+
+export const Board: Columns = {
+	backlog: {
+		name: "Backlog",
+		items: [
+			{
+				id: "100",
+				title: "Bhaiyon aur behno, to aa rha h  Manish ka dashboard ji hn Manish ka dashboard to onboard hona suru ho jao 😂",
+				description: "Arey bhai, ye toh ho gaya na ulta! Manish ke dashboard ka onBoarding itna slow hai, ki ab tak humne toh puri movie dekh li hai. Ab bas intezaar hai ki ye dashboard bhi finally onBoard ho jaye aur hum popcorn ke saath relax kar sakein. Stay tuned for more updates! ..",
+				priority: "medium",
+				deadline: 1,
+				status:"Backlog",
+				image: taskImagemani,
+				alt: "task image",
+				tags: [
+					{ title: "OnBoarding", ...getRandomColors() },
+					{ title: "SaaS", ...getRandomColors() },
+				],
+			},
+			{
+				id: uuidv4(),
+				title: "Admin Panel Back-end",
+				description: "Lorem ipsum dolor sit amet ..",
+				priority: "low",
+				deadline: 50,
+				tags: [
+					{ title: "Test", ...getRandomColors() },
+					{ title: "Front", ...getRandomColors() },
+				],
+			},
+		],
+	},
+	pending: {
+		name: "Pending",
+		items: [
+			{
+				id: uuidv4(),
+				title: "Admin Panel Back-end",
+				description: "Lorem ipsum dolor sit amet ..",
+				priority: "high",
+				deadline: 50,
+				tags: [
+					{ title: "Test", ...getRandomColors() },
+					{ title: "Front", ...getRandomColors() },
+				],
+			},
+			{
+				id: uuidv4(),
+				title: "Admin Panel Front-end",
+				description: "Lorem ipsum dolor sit amet ..",
+				priority: "low",
+				deadline: 50,
+				image: taskImage,
+				alt: "task image",
+				tags: [
+					{ title: "Test", ...getRandomColors() },
+					{ title: "Front", ...getRandomColors() },
+				],
+			},
+		],
+	},
+	todo: {
+		name: "To Do",
+		items: [
+			{
+				id: uuidv4(),
+				title: "Admin Panel Front-end",
+				description: "Lorem ipsum dolor sit amet ..",
+				priority: "medium",
+				deadline: 50,
+				image: taskImage3,
+				alt: "task image",
+				tags: [
+					{ title: "Test", ...getRandomColors() },
+					{ title: "Front", ...getRandomColors() },
+				],
+			},
+		],
+	},
+	doing: {
+		name: "Doing",
+		items: [
+			{
+				id: uuidv4(),
+				title: "Admin Panel Front-end",
+				description: "Lorem ipsum dolor sit amet ..",
+				priority: "low",
+				deadline: 50,
+				tags: [
+					{ title: "Test", ...getRandomColors() },
+					{ title: "Front", ...getRandomColors() },
+				],
+			},
+			{
+				id: uuidv4(),
+				title: "Admin Panel Back-end",
+				description: "Lorem ipsum dolor sit amet ..",
+				priority: "medium",
+				deadline: 50,
+				tags: [
+					{ title: "Test", ...getRandomColors() },
+					{ title: "Front", ...getRandomColors() },
+				],
+			},
+		],
+	},
+	done: {
+		name: "Done",
+		items: [
+			{
+				id: uuidv4(),
+				title: "Admin Panel Front-end",
+				description: "Lorem ipsum dolor sit amet ..",
+				priority: "high",
+				deadline: 50,
+				image: taskImage,
+				alt: "task image",
+				tags: [
+					{ title: "Test", ...getRandomColors() },
+					{ title: "Front", ...getRandomColors() },
+				],
+			},
+		],
+	},
+};
---
+++ src/types/index.tsx
@@ -0,0 +1,20 @@
+export type TaskT = {
+	id: string;
+	title: string;
+	description: string;
+	priority: string;
+	deadline: number;
+	status?:string;
+	image?: string;
+	alt?: string;
+	tags: { title: string; bg: string; text: string }[];
+};
+
+type Column = {
+	name: string;
+	items: TaskT[];
+};
+
+export type Columns = {
+	[key: string]: Column;
+};
